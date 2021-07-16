2f0b56f29035e43f164a2d6da3df962dfd1ee2df
mallomee <mallomee@gmail.com>
2021-07-16 11:31:40 +0200

    add bootstrap icon

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './app.scss';
+
+// == Composant
+const App = () => (
+  <div className="app">
+
+  </div>
+);
+
+// == Export
+export default App;
