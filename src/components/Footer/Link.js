aa2a1f000cc67a15de6bfe8745b166afc568de79
jcbquentin <quentin.jacob@hotmail.com>
2021-07-16 15:31:46 +0200

    footer update

---
+++ src/components/Footer/Link.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './footer.scss';
+
+const Link = () => (
+  <div className="footer__link">
+    <ul className="footer__link-group">
+      <li className="footer__link-text"> <a href="">Défense des animaux</a> </li>
+      <li className="footer__link-text"> <a href="">Nous contacter</a> </li>
+      <li className="footer__link-text"> <a href="">Mentions Légales</a> </li>
+      <li className="footer__link-text"> <a href="">CGU</a></li>
+    </ul>
+  </div>
+);
+
+export default Link;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,22 +1,16 @@
 import React from 'react';
 
 import './footer.scss';
+import Link from './Link'
 import Logo from '../../assets/logo_footer.png';
 
 const Footer = () => (
   <div>
     <div className="footer">
         <div>
-        <img src={Logo} alt="" className="footer__logo"/>
+          <img src={Logo} alt="" className="footer__logo"/>
         </div>
-      <div className="footer__link">
-        <ul className="footer__link-group">
-          <li className="footer__link-text"> <a href="">Défense des animaux</a> </li>
-          <li className="footer__link-text"> <a href="">Nous contacter</a> </li>
-          <li className="footer__link-text"> <a href="">Mentions Légales</a> </li>
-          <li className="footer__link-text"> <a href="">CGU</a></li>
-        </ul>
-      </div>
+        <Link />
     </div>
     <p className="footer__copy">NALA - 2021 &copy;</p>
   </div>
