eb377274dad06f244db557712f1dd8fc34f1e35d
mallomee <mallomee@gmail.com>
2021-07-19 16:11:00 +0200

    initialisation operationnal

--- src/index.js
+++ src/index.js
@@ -14,11 +14,9 @@ import App from 'src/components/App';
 // 1. Élément React racine (celui qui contient l'ensemble de l'app)
 //    => crée une structure d'objets imbriqués (DOM virtuel)
 const rootReactElement = (
-  <Router>
     <Provider store={store}>
       <App />
     </Provider>
-  </Router>
 );
 // 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
 const target = document.getElementById('root');
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,8 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-
-import apiMiddleware from 'src/middlewares/api';
+import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 import reducer from 'src/reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const enhancers = composeEnhancers(
-
-);
-
-const store = createStore(reducer, enhancers);
+const store = createStore(reducer, devToolsEnhancer());
 
 export default store;
