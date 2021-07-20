05c18e390847995a2c3948f00ab9a2884ff312b3
mallomee <mallomee@gmail.com>
2021-07-20 09:31:39 +0200

    correction error commit

--- src/components/Home/Carousel/index.js
+++ src/components/Home/Carousel/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 // import components
 import Card from './Card'
 import './carousel.scss';
-const Carousel = () => (
+
+const Carousel = (
+  carousel,
+  category,
+) => (
   <div className="carousel">
     <div className="carousel-nav">
-      <div className="carousel-nav-left"><i className="bi bi-chevron-compact-left"></i></div>
-      <div className="carousel-nav-right"><i className="bi bi-chevron-compact-right"></i></div>
+      <div className="carousel-nav-left" disabled={carousel[category] == 1 ? true : false}><i className="bi bi-chevron-compact-left"></i></div>
+      <div className="carousel-nav-right" disabled={carousel[category] == 10 ? true : false}><i className="bi bi-chevron-compact-right"></i></div>
     </div>
     <div className="carousel-sliders">
       <Card/>
@@ -18,4 +22,9 @@ const Carousel = () => (
   </div>
 
 );
+
+Carousel.Prototype {
+  carousel:
+}
+
 export default Carousel;
