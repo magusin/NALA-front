39af3e7b0ccda8151d97c609aca937db6c6b5fac
jcbquentin <quentin.jacob@hotmail.com>
2021-07-21 12:09:52 +0200

    fix conflict

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,6 +6,8 @@ import contactform from './contactform';
 
 const rootReducer = combineReducers({
   carousel: carousel,
+  searchbar: searchbar,
+  contactform: contactform,
 });
 
 export default rootReducer;
