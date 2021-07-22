efb2c944b8af8d46abcda3e0192505dfe5d8464f
jcbquentin <quentin.jacob@hotmail.com>
2021-07-22 11:13:25 +0200

    update page Error 404

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -5,9 +5,16 @@ import './error.scss';
 
 const Error = () => (
   <div className="error">
-    <h1 className="error__title">Oups</h1>
+    <div className="error__container">
+        <h1 className="error__container-title">Oups</h1>
+      <div className="error__container-bis">
+        <p className="error__container-text">Il n'y a rien à voir ici !</p>
+        <p className="error__container-text">Pour retourner à l'accueil</p>
+        <a href="/" className="error__container-link"><p>C'est par là !</p></a>
+      </div>
+    </div>
+    
     <div className="error__case">
-      <button className="error__button">Retour à l'accueil</button>
       <img src={Img404} alt="404 error" className="error__case-img" />
     </div>
   </div>
