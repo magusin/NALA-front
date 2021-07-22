b4d885718483e263d1c2a6d8cfadf6a8ad5d1dd4
mallomee <mallomee@gmail.com>
2021-07-22 14:08:49 +0200

    correction css and add midlewares

---
+++ src/Middlewares/posts/index.js
@@ -0,0 +1,14 @@
+import {  } from 'src/actions';
+
+const posts = (store) => (next) => (action) => {
+  switch (action.type) {
+    case :
+      
+      next(action);
+      break;
+    default:
+      next(action);
+  }
+};
+
+export default posts;
