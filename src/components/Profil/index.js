9ec535768b8c1a46522df6e16ed1218c0972b0de
magusin <fanatsy4@gmail.com>
2021-07-23 11:54:28 +0200

    correst profil scss

--- src/components/Profil/index.js
+++ src/components/Profil/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './profil.scss';
-import Card from '/src/components/Carousel/Card';
 
 
 
@@ -8,36 +7,36 @@ export default function Profil() {
   return (
     <div className="profil">
       <h1 className="profil-title">Profil</h1>
-      <section className= "profil-grid">
-          <div className="profil-grid-1">
+      <section className= "profil__section">
+          <div className="profil__section-text">
             <i className="bi bi-person-circle"></i>
           </div>
-          <div className="profil-grid-1">
+          <div className="profil__section-text">
             <p>Pseudo</p>
             <p>Nom</p>
             <p>Prénom</p>
           </div>
-          <div className="profil-grid-1">
+          <div className="profil__section-text">
             <p>Email</p>
             <p>Mot de passe</p>
             <p>Age</p>
           </div>
-          <div className="profil-grid-2">
-            <button type="button">Modifier</button>
+          <div className="profil__section-text-2">
+            <button className="profil__button" type="button">Modifier</button>
           </div>
-          <div className="profil-grid-1 profil-grid-3">
-            <button type="button">Supprimer le compte</button>
+          <div className="profil__section-text profil__section-text-3">
+            <button className="profil__button" type="button">Supprimer le compte</button>
             <input type="text"/>
           </div>
       </section>
-      <section className="profil-flex">
+      <section className="profil__section-bis">
         <img height="160" src="https://react.semantic-ui.com/images/wireframe/square-image.png" alt=""/>
         <input type="text"/>
-        <div className="profil-grid-2">
-          <button type="button">Envoyer</button>
+        <div className="profil__section-text-2">
+          <button className="profil__button" type="button">Envoyer</button>
         </div>
       </section>
-    <h2>Mes postes</h2> 
+    <h2 className="profil-title">Mes postes</h2> 
     <section className="poste">
       
     </section>
