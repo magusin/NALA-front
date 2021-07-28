33f2507d598e7c8d5fd160799cc668b3a7ff7cf5
mallomee <mallomee@gmail.com>
2021-07-28 11:14:45 +0200

    carousel done, select for categories done

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 // import components
 import Card from 'src/containers/Carousel/Card'
 import './carousel.scss';
@@ -8,25 +8,10 @@ const Carousel = (
   {
     categoryId,
     list,
-    handleNext,
-    handlePrevious,    
-    pages,
-    page
   }
 ) =>{
 
-  if(typeof pages !== 'undefined'){
-
-    console.log(pages)
-
-    if(pages.find(myPage => myPage.id == categoryId)){
-      page = myPage.page;
-    };
-
-    // console.log(element);
-  }
-  
-
+  const [page, setPage] = useState(1);
   
 
   return(
@@ -35,7 +20,7 @@ const Carousel = (
         <button 
           className="carousel-nav-left" 
           disabled={page == 1 ? true : false} 
-          onClick={() => (handlePrevious( categoryId, page ))}>
+          onClick={() => setPage(page - 1)}>
 
             <i className="bi bi-chevron-compact-left"></i>
 
@@ -44,7 +29,7 @@ const Carousel = (
         <button 
           className="carousel-nav-right" 
           disabled={page == list.length ? true : false} 
-          onClick={() => (handleNext( categoryId, page ))}>
+          onClick={() => setPage(page + 1)}>
 
             <i className="bi bi-chevron-compact-right"></i>
 
@@ -65,19 +50,14 @@ const Carousel = (
 
 Carousel.protoTypes = {
   categoryId: PropTypes.number.isRequired,
-  handlePrevious: PropTypes.func.isRequired,
-  handleNext: PropTypes.func.isRequired,
-  // list: PropTypes.arrayOf(
-  //   PropTypes.shape({
-  //     picture: PropTypes.string.isRequired,
-  //     title: PropTypes.string.isRequired,
-  //     id: PropTypes.number.isRequired
-  //   }).isRequired,
-  // ).isRequired,
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      picture: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired
+    }).isRequired,
+  ).isRequired,
 };
 
-Carousel.defaultProps = {
-  page : 1,
-}
 
 export default Carousel;
--- src/components/Nav/Selects.js
+++ src/components/Nav/Selects.js
@@ -7,11 +7,17 @@ import './nav.scss';
 const Selects = ({
   fetchCategories,
   categoriesLoaded,
-  categories
+  categories,
 }) => { 
   
-  return(
+    useEffect(
+      fetchCategories,
+      [],
+    );
 
+
+
+  return(
   <>
     <div className="nav__select">
       <span className="nav__menu">Menu &nbsp;<i className="bi bi-chevron-down"></i> </span>
@@ -19,21 +25,23 @@ const Selects = ({
           <div className="nav__select-content">
             <Link to="/" className="nav__select-link">Accueil</Link>
             <Link to="/toplove" className="nav__select-link">Top-Love</Link>
-            {categories.map((category) => {
-              {console.log(categories)}
-              <div key={category.index}>
-                {category.map((type) => (
-                  
-                    <Link key={'category' + type.id} to={`/categories/${type.id}`} className="nav__select-link">{type.name}</Link>
-                
-                ))}
-             </div>
+            {categories.map((category, index) => {
+              return(
+                <div key={index} className="nav__select-link">
+                  {category.map((type) => {
+                    {console.log(type)}             
+                    return(
+                      <Link key={'category' + type.id} to={`/categories/${type.id}`}>{type.name}</Link>
+                    )
+                  })}
+              </div>
+             )
             })}            
           </div>
         )}
     </div>
   </>
-);
+  );
 };
 
 Selects.protoTypes = {
