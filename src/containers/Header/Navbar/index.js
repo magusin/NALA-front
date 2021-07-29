7c282d794d803134940343da5b46c8011993daaf
mallomee <mallomee@gmail.com>
2021-07-29 12:14:46 +0200

    update navbar, correction button

--- src/containers/Header/Navbar/index.js
+++ src/containers/Header/Navbar/index.js
@@ -1,12 +1,19 @@
 import { connect } from 'react-redux';
 
 import Navbar from 'src/components/Header/Navbar';
+import { changeConnexionFormForConnexion, changeConnexionFormForRegister } from '../../../actions/connexionForm';
 
 const mapStateToProps = (state, ownProps) => ({
   isLogged: state.user.isLogged,
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
+  changeConnexionFormToRegister: () => {
+    dispatch(changeConnexionFormForRegister())
+  },
+  changeConnexionFormToConnexion: () => {
+    dispatch(changeConnexionFormForConnexion())
+  },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
