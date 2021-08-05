084ff307bca376c69ac0f9a7682538128833f6be
magusin <fanatsy4@gmail.com>
2021-08-05 21:13:59 +0200

    add miss passworld section - connexion

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,16 @@
 export const SELECT_INSCRIPTION_FORM = 'SELECT_INSCRIPTION_FORM';
-
 export const selectInscriptionForm = () => ({
   type: SELECT_INSCRIPTION_FORM,
 });
 
 export const SELECT_CONNEXION_FORM = 'SELECT_CONNEXION_FORM';
-
 export const selectConnexionForm = () => ({
   type: SELECT_CONNEXION_FORM,
+});
+
+export const SELECT_PASSWORD_FORM = 'SELECT_PASSWORD_FORM';
+export const selectPasswordForm = () => ({
+  type: SELECT_PASSWORD_FORM,
 });
 
 export const CHANGE_DARK_MODE = 'CHANGE_DARK_MODE';
