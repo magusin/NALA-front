6b93615c986a95ca6f70eeee7aee680cb7cc0743
Mallomee <78727080+Mallomee@users.noreply.github.com>
2021-09-05 08:28:50 +0200

    Delete animation picture and add Loading
    - delete animation picture
    - add Loading in home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,56 +8,71 @@ import Carousel from 'src/components/Carousel';
 import './home.scss';
 
 const Home = ({
-  topLoveLoaded,
-  lastPostsLoaded,
-  topLove,
-  lastPosts,
-  fetchTopLove,
-  fetchLastPosts,
-}) => {
-  useEffect(
+    topLoveLoaded,
+    lastPostsLoaded,
+    topLove,
+    lastPosts,
     fetchTopLove,
-    [],
-  );
-  
-  useEffect(
     fetchLastPosts,
-    [],
-  );
+}) => {
+    useEffect(
+        fetchTopLove, [],
+    );
 
-  return (
-    <div className="home">
+    useEffect(
+        fetchLastPosts, [],
+    );
 
-      {((lastPostsLoaded && lastPosts !== undefined)
-        && (
-        <div className="home__container">
-          <h1 className="home__title">Nos 10 dernières publications</h1>
-          <Carousel list={lastPosts} categoryId={'lastPosts'}/>
+    return ( 
+        <div className = "home">
+
+        {
+            ((lastPostsLoaded && lastPosts !== undefined) &&
+                ( 
+                    <div className = "home__container" >
+                        <h1 className = "home__title" > Nos 10 dernières publications </h1> 
+                        <Carousel 
+                            list = { lastPosts }
+                            categoryId = { 'lastPosts' }
+                        /> 
+                    </div>
+                )
+            )
+        }
+        {
+            ((lastPostsLoaded && lastPosts !== undefined) &&
+                <Loading />
+            )
+        }
+         {
+            ((topLoveLoaded && topLove !== undefined) &&
+                ( 
+                    <div className = "home__container" >
+                        <Link to = "/topLove" className = "home__title" >
+                            <h2 > TopLove </h2> 
+                        </Link> 
+                        <Carousel list = { topLove } categoryId = { 'topLove' }/> 
+                    </div>
+                )
+            )
+        }
+        {
+            ((topLoveLoaded && topLove !== undefined) &&
+                <Loading />
+            )
+        }
+
         </div>
-        )
-)}
-      {((topLoveLoaded && topLove !== undefined)
-        && (
-        <div className="home__container">
-          <Link to="/topLove" className="home__title">
-            <h2>TopLove</h2>
-          </Link>
-          <Carousel list={topLove} categoryId={'topLove'}/>
-        </div>
-        )
-)}
-
-    </div>
-  );
+    );
 };
 
 Home.propTypes = {
-  fetchTopLove: PropTypes.func.isRequired,
-  fetchLastPosts: PropTypes.func.isRequired,
-  topLoveLoaded: PropTypes.bool.isRequired,
-  lastPostsLoaded: PropTypes.bool.isRequired,
-  // topLove: PropTypes.array.isRequired,
-  // lastPosts: PropTypes.string.isRequired,
+    fetchTopLove: PropTypes.func.isRequired,
+    fetchLastPosts: PropTypes.func.isRequired,
+    topLoveLoaded: PropTypes.bool.isRequired,
+    lastPostsLoaded: PropTypes.bool.isRequired,
+    // topLove: PropTypes.array.isRequired,
+    // lastPosts: PropTypes.string.isRequired,
 };
 
 export default Home;
